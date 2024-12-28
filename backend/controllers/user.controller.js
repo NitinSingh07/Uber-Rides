e3bd8c1bd0acdeb8c8a948240f0bf0073376eaf0
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-12-28 23:56:25 +0530

    add user login functionality with validation

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,7 +2,6 @@ const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
 
-
 module.exports.registerUser = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -19,4 +18,25 @@ module.exports.registerUser = async (req, res) => {
   });
   const token = user.generateAuthToken();
   res.status(201).json({ token, user });
+};
+
+module.exports.loginUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(401).json({ message: "User not found" });
+  }
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = user.generateAuthToken();
+  res.status(200).json({ token, user });
 };
