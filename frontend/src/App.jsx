8cd6ad8c7f9b4960e36b1bbb7a27982d9d91e41d
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-12-31 16:52:57 +0530

    initialize frontend with React, Vite, and Tailwind CSS setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-green-200 underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
