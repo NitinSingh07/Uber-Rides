d9b5e0a24fca65c144290cf877f202056733581e
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-12-31 18:07:51 +0530

    update user login link color in CaptainLogin component

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -87,7 +87,7 @@ const CaptainLogin = () => {
       <div>
         <Link
           to="/user-login"
-          className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base"
+          className="bg-[#d5622d] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base"
         >
           Sign in as User
         </Link>
