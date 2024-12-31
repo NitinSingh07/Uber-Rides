5e131c557d5e3c17f49f48d514974c68bb2158ea
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-12-31 17:54:27 +0530

    add captain login page and refactor user login form for consistency

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
 
-const UserLogin = () => {
+const CaptainLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [userData, setUserData] = useState({});
+  const [captainData, setCaptainData] = useState({});
+
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    const userData = {
+    const captainData = {
       email: email,
       password: password,
     };
-    console.log("Userdata", userData);
+
+    console.log("Captaindata", captainData);
+
     setEmail("");
     setPassword("");
   };
-
   return (
     <div className="p-7 min-h-screen flex flex-col justify-between bg-white">
       {/* Logo Section */}
       <div>
         <img
           className="w-20 mb-10 mx-auto"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s"
+          src="https://www.svgrepo.com/show/505031/uber-driver.svg"
           alt="App Logo"
         />
       </div>
@@ -76,20 +78,20 @@ const UserLogin = () => {
       {/* Footer Section */}
       <div className="text-center">
         <p className="text-sm text-gray-600 mb-6">
-          New here?
-          <Link to="/user-signup" className="text-blue-600 font-medium">
-            Create a new account
+          Join our fleet?{" "}
+          <Link to="/captain-signup" className="text-blue-600 font-medium">
+            Register as a Captain
           </Link>
         </p>
         <Link
-          to="/captain-login"
-          className="bg-green-500 hover:bg-green-600 text-white font-semibold rounded-md px-4 py-3 w-full text-lg block transition duration-200"
+          to="/user-login"
+          className="bg-[#d5622d] hover:bg-[#fe7a3c] text-white font-semibold rounded-md px-4 py-3 w-full text-lg block transition duration-200"
         >
-          Sign in as Captain
+          Sign in as User
         </Link>
       </div>
     </div>
   );
 };
 
-export default UserLogin;
+export default CaptainLogin;
