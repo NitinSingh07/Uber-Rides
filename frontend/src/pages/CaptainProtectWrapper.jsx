f8de1d24d7002462cef6ef63c2e0e1093449f790
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-01 15:33:29 +0530

    enhance CaptainProtectWrapper to include loading state and fetch captain profile data

--- frontend/src/pages/CaptainProtectWrapper.jsx
+++ frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,20 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CaptainDataContext } from "../context/CaptainContext";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const CaptainProtectWrapper = ({ children }) => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
-  const { captain, setCaptain } = React.useContext(CaptainDataContext);
-
-  console.log(token);
+  const { captain, setCaptain } = useContext(CaptainDataContext);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!token) {
       navigate("/captain-login");
     }
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/captain/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setCaptain(response.data.captain);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        localStorage.removeItem("token");
+        navigate("/captain-login");
+      });
   }, [token]);
 
-  return <>{children} </>;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
 };
+
 export default CaptainProtectWrapper;
