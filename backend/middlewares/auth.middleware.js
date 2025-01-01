75385d5f388d39e90922ff3c463db0fe9505b277
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-01 15:33:19 +0530

    refactor auth middleware for improved token handling and user retrieval

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,32 +1,33 @@
 const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const captainModel = require("../models/captain.model");
 const jwt = require("jsonwebtoken");
-const blacklistTokenModel = require("../models/blacklistToken.model");
+const blackListTokenModel = require("../models/blacklistToken.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const isBlacklisted = await blacklistTokenModel.findOne({
-    token: token,
-  });
+  const isBlacklisted = await blackListTokenModel.findOne({ token: token });
 
   if (isBlacklisted) {
     return res.status(401).json({ message: "Unauthorized" });
-    }
-    
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await userModel.findById(decoded.id);
+    const user = await userModel.findById(decoded._id);
 
     req.user = user;
+
     return next();
-  } catch (error) {
+  } catch (err) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
-
 
 module.exports.authCaptain = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
