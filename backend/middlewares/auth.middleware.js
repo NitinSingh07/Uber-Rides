69c72e1c6fd107223bd5e31ce34b6ea831aa3e62
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-03 00:03:56 +0530

    enhance auth middleware by adding bcrypt and captain model imports

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const blacklistTokenModel = require("../models/blacklistToken.model");
+const captainModel = require("../models/captain.model");
+const blackListTokenModel = require("../models/blacklistToken.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
