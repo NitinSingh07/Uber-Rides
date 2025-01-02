9176a5cbdec8133d3f6344d4b75911b37c4c6e1b
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-02 10:47:28 +0530

    refactor auth middleware to improve token handling and error management

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,17 +1,16 @@
 const userModel = require("../models/user.model");
-const bcrypt = require("bcrypt");
-const captainModel = require("../models/captain.model");
 const jwt = require("jsonwebtoken");
-const blackListTokenModel = require("../models/blacklistToken.model");
+const blacklistTokenModel = require("../models/blacklistToken.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+  const isBlacklisted = await blacklistTokenModel.findOne({
+    token: token,
+  });
 
   if (isBlacklisted) {
     return res.status(401).json({ message: "Unauthorized" });
@@ -19,12 +18,11 @@ module.exports.authUser = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await userModel.findById(decoded._id);
+    const user = await userModel.findById(decoded.id);
 
     req.user = user;
-
     return next();
-  } catch (err) {
+  } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
