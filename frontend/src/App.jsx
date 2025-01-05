b2bcfae5788348d23abd89cfb311948251236f67
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-05 20:04:08 +0530

    add CaptainRiding and CaptainLogout components to App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import UserLogout from "./pages/UserLogout";
 import CaptainHome from "./pages/CaptainHome";
 import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
 import Riding from "./pages/Riding";
+import CaptainRiding from "./pages/CaptainRiding";
+import CaptainLogout from "./pages/CaptainLogout";
 
 const App = () => {
   return (
@@ -18,19 +20,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/riding" element={<Riding />} />
-
         <Route path="/user-login" element={<UserLogin />} />
         <Route path="/user-signup" element={<UserSignup />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
-        <Route
-          path="/home"
-          element={
-            <UserProtectWrapper>
-              <Home />
-            </UserProtectWrapper>
-          }
-        />
+        <Route path="/captain-riding" element={<CaptainRiding />} />
+
         <Route
           path="/captain-home"
           element={
@@ -47,20 +42,23 @@ const App = () => {
             </UserProtectWrapper>
           }
         />
-        <Route
-          path="/captain-home"
-          element={
-            <CaptainProtectWrapper>
-              <CaptainHome />
-            </CaptainProtectWrapper>
-          }
-        />
+
         <Route
           path="/user-logout"
           element={
             <UserProtectWrapper>
               <UserLogout />
             </UserProtectWrapper>
+          }
+        />
+
+        <Route
+          path="/captain-logout"
+          element={
+            <CaptainProtectWrapper>
+              {" "}
+              <CaptainLogout />
+            </CaptainProtectWrapper>
           }
         />
       </Routes>
