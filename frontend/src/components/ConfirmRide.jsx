ef8dde6d359d93272af4379c673eb87bdbfd5e33
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-08 22:22:38 +0530

    fix: update ConfirmRide component to display dynamic pickup, destination, and fare information

--- frontend/src/components/ConfirmRide.jsx
+++ frontend/src/components/ConfirmRide.jsx
@@ -11,8 +11,8 @@ const ConfirmRide = (props) => {
       >
         <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
       </h5>
-      <h3 className="text-xl font-semibold mb-5 text-center">
-        Confirm Your Ride
+      <h3 className="text-xl text-center font-semibold mb-5">
+        Confirm your Ride
       </h3>
 
       <div className="flex gap-2 justify-between flex-col items-center">
@@ -26,22 +26,22 @@ const ConfirmRide = (props) => {
             <i className="ri-map-pin-user-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              <p className="text-sm -mt-1 text-gray-600"> Shiv Gauri Vihar </p>
+              <p className="text-sm -mt-1 text-gray-600">{props.pickup}</p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3 border-b-2">
             <i className="text-lg ri-map-pin-2-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              <p className="text-sm -mt-1 text-gray-600">
-                Village Restaurant, Ramnagar Road{" "}
-              </p>
+              <p className="text-sm -mt-1 text-gray-600">{props.destination}</p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3">
             <i className="ri-currency-line"></i>
             <div>
-              <h3 className="text-lg font-medium"> ₹150</h3>
+              <h3 className="text-lg font-medium">
+                ₹{props.fare[props.vehicleType]}
+              </h3>
               <p className="text-sm -mt-1 text-gray-600">Fare</p>
             </div>
           </div>
@@ -50,6 +50,7 @@ const ConfirmRide = (props) => {
           onClick={() => {
             props.setVehicleFound(true);
             props.setConfirmRidePanel(false);
+            props.createRide();
           }}
           className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg"
         >
