e7d6182c3ac387aa80a7a8e1a3e14dcbe4c2151a
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-14 23:49:35 +0530

    fix: add remixicon font import to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,8 @@ import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
 import Riding from "./pages/Riding";
 import CaptainRiding from "./pages/CaptainRiding";
 import CaptainLogout from "./pages/CaptainLogout";
+import "remixicon/fonts/remixicon.css";
+
 
 const App = () => {
   return (
@@ -56,7 +58,6 @@ const App = () => {
           path="/captain-logout"
           element={
             <CaptainProtectWrapper>
-              {" "}
               <CaptainLogout />
             </CaptainProtectWrapper>
           }
