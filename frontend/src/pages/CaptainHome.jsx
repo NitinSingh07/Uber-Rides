df859fa2319b688bcb28d65b1879a86f6896de3c
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-14 23:37:22 +0530

    chore: remove unused SVG files and clean up CaptainHome component styling

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -126,7 +126,7 @@ const CaptainHome = () => {
       </div>
       <div
         ref={ridePopupPanelRef}
-        className="fixed w-full z-10  bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
+        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
       >
         <RidePopUp
           ride={ride}
