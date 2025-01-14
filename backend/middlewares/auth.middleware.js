4f302bace91525002612bea94d0a8b48675bf620
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-14 23:22:18 +0530

    fix: correct user ID extraction in auth middleware and streamline blacklist token check

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,18 +1,17 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const captainModel = require("../models/captain.model");
 const blackListTokenModel = require("../models/blacklistToken.model");
+const captainModel = require("../models/captain.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const isBlacklisted = await blackListTokenModel.findOne({
-    token: token,
-  });
+  const isBlacklisted = await blackListTokenModel.findOne({ token: token });
 
   if (isBlacklisted) {
     return res.status(401).json({ message: "Unauthorized" });
@@ -20,11 +19,12 @@ module.exports.authUser = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await userModel.findById(decoded.id);
+    const user = await userModel.findById(decoded._id);
 
     req.user = user;
+
     return next();
-  } catch (error) {
+  } catch (err) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
